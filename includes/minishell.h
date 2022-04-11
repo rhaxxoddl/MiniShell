1b0171d5d6f24401050e0508f03a064108cb8605
sanjeon <rhaxxoddl@naver.com>
2022-04-11 20:04:22 +0900

    ➕

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sanjeon <sanjeon@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sanjeon <sanjeon@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 11:38:46 by sanjeon           #+#    #+#             */
-/*   Updated: 2022/03/22 11:41:07 by sanjeon          ###   ########.fr       */
+/*   Updated: 2022/04/04 16:47:38 by sanjeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# include "pipe_struct.h"
 
+t_arg	*parsing(void);
 #endif
